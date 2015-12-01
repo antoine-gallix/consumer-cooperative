16eccac523f106e57558f75cf0985aad4ccec967
Antoine Gallix <antoine.gallix@gmail.com>
2015-12-01 01:21:42 +0000

    crd operation tested on user

--- code/db_access.py
+++ code/db_access.py
@@ -9,13 +9,27 @@ client = pymongo.MongoClient()
 users = client.coop.users
 
 
+class DB_Exception(Exception):
+    """Error within this module
+    """
+    pass
+
+
 def get_user(user_id):
-    """get a user from the database by its user id
+    """gets a user from the database by its user id
     """
     return users.find_one({"_id": user_id})
 
 
 def create_user(user):
-    """create a user in the database
+    """creates a user in the database
     """
     return users.insert_one(user).inserted_id
+
+
+def delete_user(user_id):
+    """deletes a user from the database by its user id. raise DB_Exception if the deletion havent been performed
+    """
+    result = users.delete_one({"_id": user_id})
+    if result.deleted_count == 0:
+        raise DB_Exception
--- code/tests/test_db_access.py
+++ code/tests/test_db_access.py
@@ -5,6 +5,7 @@ import os.path
 here_folder = os.path.dirname(__file__)
 sys.path.insert(0, here_folder + '/..')
 import db_access
+from db_access import DB_Exception
 from pdb import set_trace as bp
 
 
@@ -27,7 +28,7 @@ def test_get_user():
 
 
 def test_delete_user():
-    """create and then get a user
+    """create and then deletes a user
     """
     user = {"name": "jean-louis"}
     inserted = db_access.create_user(user)
@@ -39,5 +40,16 @@ def test_delete_user():
     assert not read
 
 
+def test_delete_error():
+    """try to delete a non existing document
+    """
+    try:
+        db_access.delete_user("mille")
+        raise Exception
+    except Exception as e:
+        # assert the correct Exception is thrown
+        assert isinstance(e, DB_Exception)
+
+
 if __name__ == '__main__':
-    test_delete_user()
+    test_delete_error()
